9962fc3fdb84a1a3aa6896471e9a03b9b5a71f4c
Matthew Lowe <lowe.matthew@mail.com>
2020-07-04 16:39:23 +0100

    Add integer and floating point defines

---
+++ Source/Core/Defines.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+// Integer definitions
+typedef int_fast8_t   Int8;
+typedef int_fast16_t  Int16;
+typedef int_fast32_t  Int32;
+typedef int_fast64_t  Int64;
+
+typedef uint_fast8_t  UInt8;
+typedef uint_fast16_t UInt16;
+typedef uint_fast32_t UInt32;
+typedef uint_fast64_t UInt64;
+
+// Floating point definitions
+typedef float  Float32;
+typedef double Float64;
